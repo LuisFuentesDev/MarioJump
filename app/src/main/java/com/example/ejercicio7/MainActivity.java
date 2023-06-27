4d5b86d9f04e467b7a974f8f6e96fa169f096a69
LuisFuentesDev <lsfuentes2@gmail.com>
2023-06-27 19:06:41 -0400

    Se soluciono el problema de fondo con las imagenes Mario salta Tiene 2 fondos de colores (verde, amarillo)

--- app/src/main/java/com/example/ejercicio7/MainActivity.java
+++ app/src/main/java/com/example/ejercicio7/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.ejercicio7;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button animateButton;
     private ImageView mario;
 
     @Override
@@ -20,23 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mario = findViewById(R.id.imageMario);
-        animateButton = findViewById(R.id.buttonJump);
+        findViewById(R.id.buttonJump);
+        Toast.makeText(getApplicationContext(), "Bienvenido", Toast.LENGTH_SHORT).show();
     }
 
-
     public void onAnimateButtonClick(View view) {
-        Animation slideUp = new TranslateAnimation(0, 0, 0, -200);
+        Animation slideUp = new TranslateAnimation(0, 0, 0, -150);
         slideUp.setDuration(500);
         slideUp.setFillAfter(true);
 
-        Animation slideDown = new TranslateAnimation(0, 0, 0, 0);
-        slideDown.setDuration(500);
-        slideDown.setFillAfter(true);
-
         final AnimationSet animationSet = new AnimationSet(true);
         animationSet.addAnimation(slideUp);
-        animationSet.addAnimation(slideDown);
 
         mario.startAnimation(animationSet);
     }
+
 }
